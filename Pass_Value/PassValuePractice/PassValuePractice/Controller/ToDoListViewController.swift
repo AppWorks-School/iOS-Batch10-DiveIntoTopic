3886700119cc750b9128a142e6a9d79e1ab84716
Luke <wuchiwei17@gmail.com>
2019-08-19 18:13:25 +0800

    Add new class AddItemViewController and ToDoListViewController.
    renamed:    PassValuePractice/PassValuePractice/ViewController.swift -> PassValuePractice/PassValuePractice/Controller/AddItemViewController.swift
    new file:   PassValuePractice/PassValuePractice/Controller/ToDoListViewController.swift

---
+++ Pass_Value/PassValuePractice/PassValuePractice/Controller/ToDoListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ToDoListViewController.swift
+//  PassValuePractice
+//
+//  Created by WU CHIH WEI on 2019/8/19.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    var datas: [String] = ["1", "2", "3", "4"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+extension ToDoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = datas[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension ToDoListViewController: UITableViewDelegate {
+    
+}
